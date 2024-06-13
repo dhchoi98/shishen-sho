b15d62fdb206edda367fa8fb201857da0d2a8a44
dhchoi98 <chleogks9808@naver.com>
2024-06-14 00:18:17 +0900

    [FEAT] 방 대기화면, 방 조회화면 UI 및 기능 구현 (UDP 통신)

---
+++ shishen-sho/MultiPlay_InRoom.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace shishen_sho
+{
+    partial class MultiPlay_InRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.player1Name = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 139);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(81, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "플레이어 1 :";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(257, 129);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(92, 39);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "준비";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(429, 139);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(144, 62);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "게임시작";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(429, 247);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(144, 65);
+            this.metroButton3.TabIndex = 3;
+            this.metroButton3.Text = "나가기";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 293);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "플레이어 2 :";
+            this.metroLabel2.Click += new System.EventHandler(this.metroLabel2_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(257, 285);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(92, 37);
+            this.metroButton4.TabIndex = 5;
+            this.metroButton4.Text = "준비";
+            this.metroButton4.UseSelectable = true;
+            // 
+            // player1Name
+            // 
+            this.player1Name.AutoSize = true;
+            this.player1Name.Location = new System.Drawing.Point(141, 139);
+            this.player1Name.Name = "player1Name";
+            this.player1Name.Size = new System.Drawing.Size(37, 19);
+            this.player1Name.TabIndex = 6;
+            this.player1Name.Text = "이름";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(141, 293);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(37, 19);
+            this.metroLabel4.TabIndex = 7;
+            this.metroLabel4.Text = "이름";
+            // 
+            // MultiPlay_InRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 403);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.player1Name);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "MultiPlay_InRoom";
+            this.Text = "방 대기화면";
+            this.Load += new System.EventHandler(this.MultiPlay_CreateRoom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroLabel player1Name;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+    }
+}
---
+++ shishen-sho/MultiPlay_InRoom.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shishen_sho
+{
+    public partial class MultiPlay_InRoom : MetroFramework.Forms.MetroForm
+    {
+        private string username;
+        private MultiPlay multiplay;
+        private MultiPlay_ShowRoom showRoom;
+        private bool host = false; // 방장일 때
+
+        private UdpClient udpClient;
+        private string hostIpAddress;
+ 
+        private Thread listenThread;
+
+        public MultiPlay_InRoom(string username, MultiPlay multiPlay) // 방장
+        {
+            InitializeComponent();
+            this.username = username;
+            this.multiplay = multiPlay;
+            host = true;
+            // 방 생성 후 브로드캐스트 메시지 전송
+            StartBroadcasting();
+        }
+        public MultiPlay_InRoom(string username, MultiPlay_ShowRoom showRoom, string ipAddress) // 게스트
+        {
+            InitializeComponent();
+            this.username = username;
+            this.showRoom = showRoom;
+            this.hostIpAddress = ipAddress;
+        }
+
+        private void StartBroadcasting()
+        {
+            Thread broadcastThread = new Thread(new ThreadStart(BroadcastHost));
+            broadcastThread.IsBackground = true;
+            broadcastThread.Start();
+        }
+
+        private void BroadcastHost()
+        {
+
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.EnableBroadcast = true;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 8888);
+            byte[] message = Encoding.ASCII.GetBytes("I am the host");
+
+            while (true)
+            {
+                udpClient.Send(message, message.Length, endPoint);
+                Thread.Sleep(1000); // 1초마다 브로드캐스트
+                Console.WriteLine("브로드 캐스트");
+            }
+        }
+        private void MultiPlay_CreateRoom_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            multiplay.Show();
+            this.Close();
+        }
+   
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
