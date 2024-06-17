aaf8e498401531f0ae0bab947f1a9c10785f44f0
EunbinKim <kdmsqls03@gmail.com>
2024-06-17 14:26:30 +0900

    점수판 구현 - mode와 score listView 형태로 표시

--- shishen-sho/Score.cs
+++ shishen-sho/Score.cs
@@ -15,11 +15,44 @@ namespace shishen_sho
         public Score()
         {
             InitializeComponent();
+            InitializeListView();
         }
-
+        private void InitializeListView()
+        {
+            // 열 추가 및 너비 설정
+            ListViewScore.Columns.Add("Mode", 100, HorizontalAlignment.Left);
+            ListViewScore.Columns.Add("Score", 100, HorizontalAlignment.Left);
+            ListViewScore.GridLines = true; // 그리드 라인 활성화
+            ListViewScore.View = View.Details; // 상세 보기 모드
+        }
         private void Score_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        public void AddScore(int score, int mode)
+        {
+            
+            // Mode 항목 추가
+            string modeText = null;
+            if (mode == 1)
+            {
+                modeText = "Easy";
+            }
+            else if (mode == 2)
+            {
+                modeText = "Normal";
+            }
+            else
+            {
+                modeText = "Hard";
+            }
+
+            ListViewItem scoreItem = new ListViewItem(modeText); // Score 값을 첫 번째 열에 추가
+
+            scoreItem.SubItems.Add(score.ToString()); // Mode 값을 두 번째 열에 추가
+
+            ListViewScore.Items.Add(scoreItem); 
         }
     }
 }
